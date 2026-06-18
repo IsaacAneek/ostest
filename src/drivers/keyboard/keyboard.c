0e46328fa1545768e72f03f8d6fd932e94b7204e
IsaacAneek <IsaacAneek@protonmail.com>
2026-06-18 21:49:36 +0600

    fixed newline bug where pressing enter wasnt entering a newline

--- src/drivers/keyboard/keyboard.c
+++ src/drivers/keyboard/keyboard.c
@@ -14,7 +14,7 @@ static char key_buffer[256];
 #define SC_MAX 57
 const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
-        'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
+        'U', 'I', 'O', 'P', '[', ']', '\n', '?', 'A', 'S', 'D', 'F', 'G', 
         'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
         'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
 
@@ -33,9 +33,9 @@ void ISR_keyboard_handler_01()
         //kprint_backspace();
         backspace();
     } else if (scancode == ENTER) {
-        putc_vga("\n");
-        //user_input(key_buffer); /* kernel-controlled function */
-        key_buffer[0] = '\0';
+        putc_vga('\n');
+        // user_input(key_buffer); /* kernel-controlled function */
+        // key_buffer[0] = '\0';
     } else {
         char letter = sc_ascii[(int)scancode];
         /* Remember that kprint only accepts char[] */
